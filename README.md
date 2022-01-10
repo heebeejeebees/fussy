e23d5271463cd3778f5c8b7616c2b07d639f59a6
jaaviieerx <javiertjh@gmail.com>
2022-01-10 18:54:53 +0800

    initial draft commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fussy
+A smart marketplace where end-users manipulate database columns from frontend
---
+++ src/test/java/com/fussy/fussy/FussyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fussy.fussy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FussyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
